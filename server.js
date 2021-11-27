35763562dad147a30a7e535cb0406226d704d59c
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-11-26 21:54:15 -0600

    Completed

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require( 'express' );
 const app = express();
-const session = require('express-session');
-const flash = require('express-flash');
 const { ApiNameRouter } = require( './server/routes/apiNameRouter' );
 
 require( './server/config/database' );
 
 app.set('views', __dirname + '/client/views'); 
 app.set('view engine', 'ejs');
-app.use(flash());
 
 app.use( express.urlencoded({ extended: true }) );
 app.use(express.json());
